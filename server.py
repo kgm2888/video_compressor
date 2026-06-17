3a218a7218a6b551c5b72432ea4de586b1ab3048
{中浦佐和｝ <{sawasawa003838@gmail.com}>
2026-06-17 10:07:34 +0900

    Add: TCPサーバの起動処理を追加

---
+++ server.py
@@ -0,0 +1,23 @@
+import socket
+
+HOST = "0.0.0.0"
+PORT = 9000
+
+
+def start_server():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        server_socket.bind((HOST, PORT))
+        server_socket.listen()
+
+        print(f"Server listening on {HOST}:{PORT}")
+
+        while True:
+            conn, addr = server_socket.accept()
+            print(f"Connected by {addr}")
+            conn.close()
+
+
+if __name__ == "__main__":
+    start_server()
